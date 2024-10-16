c3b862169332c83afcfb6cbe636ec1e608e82c64
210854M <210971@utags.edu.mx>
2024-10-16 00:40:22 -0600

    Update procesar.php

--- procesar.php
+++ procesar.php
@@ -62,7 +62,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $mail->Host = 'smtp.gmail.com';  // Servidor SMTP de Gmail
                 $mail->SMTPAuth = true;
                 $mail->Username = 'yosivelasco123@gmail.com';  // Tu dirección de correo Gmail
-                $mail->Password = 'volkway16';  // Tu App Password de Gmail
+                $mail->Password = 'tmnphqpuctahfksuj';  // Tu App Password generada por Google
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
                 $mail->Port = 587;  // Puerto TLS
 
